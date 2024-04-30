0c064b6342ad20952ad485cf39c211e383b2e9dd
ruslantilovov2 <ruslantilovov2@gmail.com>
2024-04-30 11:18:48 +0500

    begin project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl text-red-200 font-bold underline">Hello world!</h1>
     </div>
   );
 };
